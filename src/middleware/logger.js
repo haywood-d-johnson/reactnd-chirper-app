f995c5b1241d16b3b1dd8e0f6c3e3ff57f9c7b15
haywood-d-johnson <haywood.d.johnson@gmail.com>
2020-10-13 17:56:58 -0500

    feat: store connected to App component

--- src/middleware/logger.js
+++ src/middleware/logger.js
@@ -1,10 +1,10 @@
 const logger = (store) => (next) => (action) => {
   console.group(action.type);
-  console.log("The action: ", action);
-  const returnVal = next(action);
-  console.log("The new state: ", store.getState());
+  console.log("The action:", action);
+  const returnValue = next(action);
+  console.log("The new state:", store.getState());
   console.groupEnd();
-  return returnVal;
+  return returnValue;
 };
 
 export default logger;
