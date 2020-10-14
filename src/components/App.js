86d59169d3bb88a46583581f00d2594ee2046fc8
haywood-d-johnson <haywood.d.johnson@gmail.com>
2020-10-13 19:21:47 -0500

    feat: renders tweet id; logger shows store

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,21 @@ import { connect } from "react-redux";
 
 import { handleInitialData } from "../actions/shared";
 
+import Dashboard from "./Dashboard";
+
 class App extends Component {
   componentDidMount() {
     this.props.dispatch(handleInitialData());
   }
   render() {
-    return <div>Starter Code</div>;
+    return <div>{this.props.loading === true ? null : <Dashboard />}</div>;
   }
 }
 
-export default connect()(App);
+function mapStateToProps({ authedUser }) {
+  return {
+    loading: authedUser === null,
+  };
+}
+
+export default connect(mapStateToProps)(App);
