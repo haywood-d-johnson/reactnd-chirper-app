f9db606947f85ca3356364c8eb37c60f24ee175a
haywood-d-johnson <haywood.d.johnson@gmail.com>
2020-10-23 03:32:42 -0500

    feat: adds and renders tweet and tweet replies

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import LoadingBar from "react-redux-loading";
 import { handleInitialData } from "../actions/shared";
 import Dashboard from "./Dashboard";
 import NewTweet from "./NewTweet";
+import TweetPage from "./TweetPage";
 
 class App extends Component {
   componentDidMount() {
@@ -14,7 +15,9 @@ class App extends Component {
     return (
       <div>
         <LoadingBar />
-        {this.props.loading === true ? null : <NewTweet />}
+        {this.props.loading === true ? null : (
+          <TweetPage match={{ params: { id: "8xf0y6ziyjabvozdd253nd" } }} />
+        )}
       </div>
     );
   }
